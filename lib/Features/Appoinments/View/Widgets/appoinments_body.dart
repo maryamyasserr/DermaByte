e334c62147ff9f3de9620c4400bbd7996683ef13
Seif Tariq <SeifTariq@gmail.com>
2023-11-18 18:27:46 +0200

    create appoinments feature folder

---
+++ lib/Features/Appoinments/View/Widgets/appoinments_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppoinmentsBody extends StatelessWidget {
+  const AppoinmentsBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column();
+  }
+}
---
+++ lib/Features/Appoinments/View/appoinments.dart
@@ -0,0 +1,13 @@
+import 'package:dermabyte/Features/Appoinments/View/Widgets/appoinments_body.dart';
+import 'package:flutter/material.dart';
+
+class Appoinments extends StatelessWidget {
+  const Appoinments({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: AppoinmentsBody(),
+    );
+  }
+}
