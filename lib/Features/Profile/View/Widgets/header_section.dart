47bc2e992f015e33a1b848124d09962d8435f8af
Maryam <maryammyasserr912@gmail.com>
2023-11-22 18:52:16 +0200

    config

--- lib/Features/Profile/View/Widgets/header_section.dart
+++ lib/Features/Profile/View/Widgets/header_section.dart
@@ -8,42 +8,6 @@ class HeaderSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
-<<<<<<< HEAD
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Row(
-          children: [
-            ProfilePhoto(),
-            SizedBox(
-              width: 20,
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Ahmed Mohamed",
-                  style: Styels.textStyle20_700,
-                ),
-                Text(
-                  "patient",
-                  style: Styels.textStyle14_300,
-                ),
-              ],
-            ),
-          ],
-        ),
-        Padding(
-          padding: const EdgeInsets.all(0),
-          child: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.edit_outlined,
-                size: 30,
-              )),
-        )
-      ],
-    );
-=======
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
                Row(
@@ -79,6 +43,5 @@ class HeaderSection extends StatelessWidget {
               )
             ],
           );
->>>>>>> 19c63cb5047fbbb1e0b61c84f2f8a82c72177ea7
   }
 }
