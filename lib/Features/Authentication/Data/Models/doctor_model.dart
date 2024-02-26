47b1a8382d64db3571b1751a19972d41a83cb4fd
Seif Tariq <SeifTariq@gmail.com>
2024-02-26 17:25:02 +0200

    config

--- lib/Features/Authentication/Data/Models/doctor_model.dart
+++ lib/Features/Authentication/Data/Models/doctor_model.dart
@@ -1,52 +1,57 @@
 class DoctorModel {
-  String? firstName;
-  String? lastName;
-  String? mobile;
-  String? location;
-  String? city;
-  String? country;
-  String? specialization;
-  String? license;
-  String? email;
-  String? password;
-  String? passwordConfirm;
-  String? profilePic;
-  int? sessionCost;
-  bool? state;
+  String firstName;
+  String lastName;
+  String mobile;
+  String location;
+  String city;
+  String country;
+  String specialization;
+  String license;
+  String email;
+  String password;
+  String passwordConfirm;
+  String profilePic;
+  int sessionCost;
+  bool state;
+  DateTime? createdAt;
+  DateTime? updatedAt;
 
-  DoctorModel({
-    this.firstName,
-    this.lastName,
-    this.mobile,
-    this.location,
-    this.city,
-    this.country,
-    this.specialization,
-    this.license,
-    this.email,
-    this.password,
-    this.passwordConfirm,
-    this.profilePic,
-    this.sessionCost,
-    this.state,
-  });
+  DoctorModel(
+      {required this.firstName,
+      required this.lastName,
+      required this.mobile,
+      required this.location,
+      required this.city,
+      required this.country,
+      required this.specialization,
+      required this.license,
+      required this.email,
+      required this.password,
+      required this.passwordConfirm,
+      required this.profilePic,
+      required this.sessionCost,
+      required this.state,
+      this.createdAt,
+      this.updatedAt
+      });
 
   factory DoctorModel.fromJson(Map<String, dynamic> json) => DoctorModel(
-        firstName: json['firstName'] as String?,
-        lastName: json['lastName'] as String?,
-        mobile: json['mobile'] as String?,
-        location: json['location'] as String?,
-        city: json['city'] as String?,
-        country: json['country'] as String?,
-        specialization: json['specialization'] as String?,
-        license: json['license'] as String?,
-        email: json['email'] as String?,
-        password: json['password'] as String?,
-        passwordConfirm: json['passwordConfirm'] as String?,
-        profilePic: json['profilePic'] as String?,
-        sessionCost: json['sessionCost'] as int?,
-        state: json['state'] as bool?,
-      );
+      firstName: json['firstName'] as String,
+      lastName: json['lastName'] as String,
+      mobile: json['mobile'] as String,
+      location: json['location'] as String,
+      city: json['city'] as String,
+      country: json['country'] as String,
+      specialization: json['specialization'] as String,
+      license: json['license'] as String,
+      email: json['email'] as String,
+      password: json['password'] as String,
+      passwordConfirm: json['passwordConfirm'] as String,
+      profilePic: json['profilePic'] as String,
+      sessionCost: json['sessionCost'] as int,
+      state: json['state'] as bool,
+      createdAt: json['createdAt'] as DateTime?,
+      updatedAt: json['updatedAt'] as DateTime?);
 
   Map<String, dynamic> toJson() => {
         'firstName': firstName,
--- lib/Features/Authentication/Data/Models/lap_model.dart
+++ lib/Features/Authentication/Data/Models/lap_model.dart
@@ -1,39 +1,39 @@
 class LapModel {
-  String? name;
-  String? mobile;
-  String? location;
-  String? city;
-  String? country;
-  String? license;
-  String? email;
-  String? password;
-  String? passwordConfirm;
-  bool? state;
+  String name;
+  String mobile;
+  String location;
+  String city;
+  String country;
+  String license;
+  String email;
+  String password;
+  String passwordConfirm;
+  bool state;
 
   LapModel({
-    this.name,
-    this.mobile,
-    this.location,
-    this.city,
-    this.country,
-    this.license,
-    this.email,
-    this.password,
-    this.passwordConfirm,
-    this.state,
+   required this.name,
+   required this.mobile,
+   required this.location,
+   required this.city,
+   required this.country,
+   required this.license,
+   required this.email,
+   required this.password,
+   required this.passwordConfirm,
+   required this.state,
   });
 
   factory LapModel.fromJson(Map<String, dynamic> json) => LapModel(
-        name: json['name'] as String?,
-        mobile: json['mobile'] as String?,
-        location: json['location'] as String?,
-        city: json['city'] as String?,
-        country: json['country'] as String?,
-        license: json['license'] as String?,
-        email: json['email'] as String?,
-        password: json['password'] as String?,
-        passwordConfirm: json['passwordConfirm'] as String?,
-        state: json['state'] as bool?,
+        name: json['name'] as String,
+        mobile: json['mobile'] as String,
+        location: json['location'] as String,
+        city: json['city'] as String,
+        country: json['country'] as String,
+        license: json['license'] as String,
+        email: json['email'] as String,
+        password: json['password'] as String,
+        passwordConfirm: json['passwordConfirm'] as String,
+        state: json['state'] as bool,
       );
 
   Map<String, dynamic> toJson() => {
