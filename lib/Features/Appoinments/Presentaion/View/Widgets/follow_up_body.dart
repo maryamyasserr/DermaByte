31cba041e1b91c2cf68a063b71f4e4ab686ccc4b
Seif Tariq <SeifTariq@gmail.com>
2024-02-27 20:32:24 +0200

    apply blocBuilder in follow up body

--- lib/Features/Appoinments/Presentaion/View/Widgets/follow_up_body.dart
+++ lib/Features/Appoinments/Presentaion/View/Widgets/follow_up_body.dart
@@ -1,9 +1,13 @@
 import 'package:dermabyte/Core/Widgets/custom_appbar.dart';
+import 'package:dermabyte/Core/Widgets/err_widget.dart';
+import 'package:dermabyte/Core/Widgets/loading_indicator.dart';
 import 'package:dermabyte/Core/utils/assets.dart';
 import 'package:dermabyte/Features/Appoinments/Presentaion/View/Widgets/custom_card.dart';
 import 'package:dermabyte/Features/Appoinments/Presentaion/View/Widgets/header_text.dart';
+import 'package:dermabyte/Features/Appoinments/Presentaion/View_Model/Cubits/Preservation_Cubit/preservation_info_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FollowUpBody extends StatelessWidget {
   const FollowUpBody({super.key});
@@ -22,22 +26,34 @@ class FollowUpBody extends StatelessWidget {
             const HeaderText(
                 text: "Follow up with your ongoing and future appointments."),
             const SizedBox(height: 32),
-            Expanded(
-                child: ListView.builder(
-                    itemCount: 10,
-                    physics: const BouncingScrollPhysics(),
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 12),
-                        child: CustomCard(
-                          iconCard: Assets.kFollowUpIcon,
-                          cardTitle: "Dr. Mai",
-                          cardSubTitle: " Dr. Mai has reviewed your scans andDr. Mai has reviewed your scans and",
-                          onPressed: () {},
-                          textButton: "View",
-                        ),
-                      );
-                    }))
+            BlocBuilder<PreservationInfoCubit, PreservationInfoState>(
+              builder: (context, state) {
+                if (state is PreservationInfoFailure) {
+                  return const ErrWidget();
+                } else if (state is PreservationInfoSuccess) {
+                  return Expanded(
+                      child: ListView.builder(
+                          itemCount: state.pReservationInfo.length,
+                          physics: const BouncingScrollPhysics(),
+                          itemBuilder: (context, index) {
+                            return Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 12),
+                              child: CustomCard(
+                                iconCard: Assets.kFollowUpIcon,
+                                cardTitle:
+                                    "Dr ${state.pReservationInfo[index].dermatologist.firstName}",
+                                cardSubTitle:
+                                    " Dr. ${state.pReservationInfo[index].dermatologist.firstName} has reviewed your scans",
+                                onPressed: () {},
+                                textButton: "View",
+                              ),
+                            );
+                          }));
+                } else {
+                  return const LoadingIndicator();
+                }
+              },
+            )
           ],
         ),
       ),
--- lib/Features/Appoinments/Presentaion/View/appoinments_view.dart
+++ lib/Features/Appoinments/Presentaion/View/appoinments_view.dart
@@ -2,6 +2,7 @@ import 'package:dermabyte/Core/utils/service_locator.dart';
 import 'package:dermabyte/Features/Appoinments/Data/Repo/preservation_info_repo_impl.dart';
 import 'package:dermabyte/Features/Appoinments/Presentaion/View/Widgets/appoinments_body.dart';
 import 'package:dermabyte/Features/Appoinments/Presentaion/View_Model/Cubits/Preservation_Cubit/preservation_info_cubit.dart';
+import 'package:dermabyte/Features/Authentication/Presentation/View%20Model/Auth%20Cubit/auth_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -11,7 +12,8 @@ class AppoinmentsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => PreservationInfoCubit(getIt.get<PreservationInfoRepoImpl>()),
+      create: (context) => PreservationInfoCubit(getIt.get<PreservationInfoRepoImpl>())
+      ..getPatientReservationInfo(id: BlocProvider.of<AuthCubit>(context).patientModel!.id),
       child: const AppoinmentsBody(),
     );
   }
