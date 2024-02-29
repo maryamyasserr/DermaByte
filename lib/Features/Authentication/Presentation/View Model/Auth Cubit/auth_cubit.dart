010ff5ce1396d54f0606cb360311bd6aa92e39f6
Seif Tariq <SeifTariq@gmail.com>
2024-02-29 13:38:36 +0200

    c

--- lib/Features/Authentication/Presentation/View Model/Auth Cubit/auth_cubit.dart
+++ lib/Features/Authentication/Presentation/View Model/Auth Cubit/auth_cubit.dart
@@ -4,49 +4,55 @@ import 'package:dermabyte/Features/Authentication/Data/Models/lap_model.dart';
 import 'package:dermabyte/Features/Authentication/Data/Models/patient_model.dart';
 import 'package:dermabyte/Features/Authentication/Data/Repo/auth_repo.dart';
 import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
 
 part 'auth_state.dart';
 
 class AuthCubit extends Cubit<AuthState> {
-  AuthCubit(this.authRepo) : super(AuthInitial());
   AuthRepo authRepo;
+  AuthCubit(this.authRepo) : super(AuthInitial());
 
   PatientModel? patientModel;
   DoctorModel? doctorModel;
   LabModel? labModel;
+  bool? isLoading;
 
   Future<void> signUp(
       {required dynamic data,
       @required String? token,
       required BuildContext context,
       required String role}) async {
+    isLoading = true;
     emit(AuthLoading());
     if (role == 'patient') {
-      var response = await authRepo.signUpAsPatient(
-          data: data, token: token, context: context);
+      var response = await authRepo.signUpAsPatient(data: data, token: token,context:context );
       response.fold((failure) {
         emit(AuthFailure(errMessage: failure.errMessage));
+        isLoading = false;
       }, (patient) {
         patientModel = patient;
         emit(AuthSuccess());
+        isLoading = false;
       });
     } else if (role == 'doctor') {
-      var response = await authRepo.signUpAsDsoctor(
-          data: data, token: token, context: context);
+      var response = await authRepo.signUpAsDsoctor(data: data, token: token);
       response.fold((failure) {
         emit(AuthFailure(errMessage: failure.errMessage));
+        isLoading = false;
       }, (doctor) {
         doctorModel = doctor;
         emit(AuthSuccess());
+        isLoading = false;
       });
     } else {
-      var response = await authRepo.signUpAsLap(
-          data: data, token: token, context: context);
+      var response = await authRepo.signUpAsLap(data: data, token: token);
       response.fold((failure) {
         emit(AuthFailure(errMessage: failure.errMessage));
+        isLoading = false;
       }, (lap) {
         labModel = lap;
         emit(AuthSuccess());
+        isLoading = false;
       });
     }
   }
