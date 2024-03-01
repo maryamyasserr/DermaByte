3a68b343279d298379c41df16c53c078627a3ee0
Seif Tariq <SeifTariq@gmail.com>
2024-03-01 21:44:06 +0200

    apply repo pattern

--- lib/Features/Scan/Data/scan.dart
+++ lib/Features/Scan/Data/scan.dart
@@ -1,45 +1,31 @@
 class Scan {
   String? id;
-  DateTime? scanDate;
-  String? diseasePhoto;
-  String? diseaseName;
-  String? patient;
-  DateTime? createdAt;
-  DateTime? updatedAt;
+  DateTime scanDate;
+  String diseasePhoto;
+  String diseaseName;
+  String patient;
 
   Scan({
     this.id,
-    this.scanDate,
-    this.diseasePhoto,
-    this.diseaseName,
-    this.patient,
-    this.createdAt,
-    this.updatedAt,
+    required this.scanDate,
+    required this.diseasePhoto,
+    required this.diseaseName,
+    required this.patient,
   });
 
   factory Scan.fromJson(Map<String, dynamic> json) => Scan(
         id: json['id'] as String?,
-        scanDate: json['scanDate'] == null
-            ? null
-            : DateTime.parse(json['scanDate'] as String),
-        diseasePhoto: json['diseasePhoto'] as String?,
-        diseaseName: json['diseaseName'] as String?,
-        patient: json['patient'] as String?,
-        createdAt: json['createdAt'] == null
-            ? null
-            : DateTime.parse(json['createdAt'] as String),
-        updatedAt: json['updatedAt'] == null
-            ? null
-            : DateTime.parse(json['updatedAt'] as String),
+        scanDate: DateTime.parse(json['scanDate'] as String),
+        diseasePhoto: json['diseasePhoto'] as String,
+        diseaseName: json['diseaseName'] as String,
+        patient: json['patient'] as String,
       );
 
   Map<String, dynamic> toJson() => {
         'id': id,
-        'scanDate': scanDate?.toIso8601String(),
+        'scanDate': scanDate.toIso8601String(),
         'diseasePhoto': diseasePhoto,
         'diseaseName': diseaseName,
         'patient': patient,
-        'createdAt': createdAt?.toIso8601String(),
-        'updatedAt': updatedAt?.toIso8601String(),
       };
 }
